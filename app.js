5d98897c309f80e7c5c11f88966f549d508510a6
Rahul Negi <44065254+insignificantGuy@users.noreply.github.com>
2022-04-28 17:37:57 +0530

    Update App.js
    Added Comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+
+// To Start the App, just type node app.js and wait untill it finishes the calculations.
+
+//Using third party I converted the .csv into JSON file for easy parsing.
 const reactionsJsonData = require('./data/reactions.json');
 const JobsJsonData = require('./data/jobs.json');
 
+//Importing the functions that are asked in the Question Statement.
 const {getSimilarityScores} = require('./task1Functions');
 const getCompanySimilarityScore = require('./task2Functions');
 
