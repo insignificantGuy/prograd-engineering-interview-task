40e67adc6ae0b36190f10763c8ae5e7be57a8ea9
Rahul Negi <44065254+insignificantGuy@users.noreply.github.com>
2022-04-28 17:39:20 +0530

    Update task2Functions.js
    Added Comments for better readability

--- task2Functions.js
+++ task2Functions.js
@@ -1,6 +1,25 @@
 const {UserLikedJobs} = require('./task1Functions'); 
 const {getAllNumberOfMatches} = require('./task1Functions');
 
+// ----------------------------------------------------------------------
+/*
+Algorithm Explanation of Task 2
+
+This one was a little bit difficult and I have to think really hard on it.
+1. The first step was to segregate the companies based on the jobs. Like a company could have several job, so I segreagated them using
+getCompaniesByJobs function.
+
+2. Now as per question we need to find out user who have liked at least one job of a company. So I looped over
+All User who liked jobs obj and All companies and their job object. I looped over the kept a constant array of object where I stored the company_id
+and the people who have liked jobs at those companies.
+
+3. Now we have data of company, poeple who liked jobs at those company. Now simply I looped over the object array 
+and kept a count of maximumSimilarityScore and companies have these similarity Score. Finally I printed them.
+
+*/
+//-----------------------------------------------------------------------
+
+// Function to get All companies based on jobs
 function getCompaniesByJobs(jobsJsonData){
     var companiesByJobs = {};
     jobsJsonData.forEach((element) => {
@@ -14,12 +33,12 @@ function getCompaniesByJobs(jobsJsonData){
     return companiesByJobs;
 };
 
+
+// Function to get All Applicant who has liked one job
 function getAllCompanyApplicants(reactionsJsonData, jobsJsonData){
     const AllUserLikedJobsObj = UserLikedJobs(reactionsJsonData);
     const companiesByJobs = getCompaniesByJobs(jobsJsonData);
-    const comapnyApplicantObj = {
-        company_id:[]
-    }
+    const comapnyApplicantObj = {}
 
     for(let company_id in companiesByJobs){
         for(let user_id in AllUserLikedJobsObj){
@@ -35,10 +54,10 @@ function getAllCompanyApplicants(reactionsJsonData, jobsJsonData){
             }
         }
     }
-    
     return comapnyApplicantObj;
 }
 
+// Main Program that calculates the company Similarity Score
 module.exports = function getCompanySimilarityScore(reactionsJsonData, jobsJsonData){
     const AllCompanyApplicants = getAllCompanyApplicants(reactionsJsonData, jobsJsonData);
     var maximumNumberOfMatches = 0;
@@ -59,7 +78,7 @@ module.exports = function getCompanySimilarityScore(reactionsJsonData, jobsJsonD
             }
         }
     }
-    
+
     similarityScore.currentCompanies = similarityScore.currentCompanies.filter((element, index) => similarityScore.currentCompanies.indexOf(element) === index);
     console.log(`The Highest Similarity Score between two companies is ${similarityScore.maxSimilarityScore} and it is between companies ${similarityScore.currentCompanies}`);
 }
