2a636d58dc5a8bea9bc5cfef01cd211f40ba4f17
Rahul Negi <44065254+insignificantGuy@users.noreply.github.com>
2022-04-28 17:38:48 +0530

    Update Function 1
    Added comments for better Readability

--- task1Functions.js
+++ task1Functions.js
@@ -1,3 +1,23 @@
+// ----------------------------------------------------------------------
+/*
+Algorithm Explanation of Task 1
+
+This one was simple but harder to imagine. Fortunately I was able to do it Easily.
+1. The first step was to segregate the user based on the Jobs they liked. Now we only needed those jobs
+that have direction as true, So I used a filter and stored all those data in an object.
+
+2. Next, we needed a function that would calculate the Number of Matches. So I created a function that uses 2
+for loops. We needed only the count, so I filtered and sorted the array and returned it's length.
+
+3. According to question we needed people having maximum similarity score. We currently have object of object
+that contains userId and jobs they have liked. Now simply I looped over the object of object
+and kept a count of maximumSimilarityScore and Users have these similarity Score. Finally I printed them.
+
+*/
+//-----------------------------------------------------------------------
+
+
+
 // Function to get All Jobs That are Liked By User
 function UserLikedJobs(userObj){
     var UserLikedJobsObj = {};
@@ -15,6 +35,7 @@ function UserLikedJobs(userObj){
     return UserLikedJobsObj;
 }
 
+// Function to get All the number of Matches in two Array containing Liked Instance
 function getAllNumberOfMatches(userArray1, userArray2){
     var totalNumberofMatches = [];
     for(var i = 0; i<userArray1.length;i++){
@@ -29,6 +50,7 @@ function getAllNumberOfMatches(userArray1, userArray2){
     return sortedFilteredTotalNumberOfMatches.length;
 }
 
+// Main Program that calculates the similarity Scores between two User.
 function getSimilarityScores(reactionsJsonData){
     const AllUserLikedJobs = UserLikedJobs(reactionsJsonData);
     let maximumNumberofMatches = 0;
